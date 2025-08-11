44b1884ab4cb999345ff50cb9658b2808419727e
ailchenko <velma86@bk.ru>
2025-08-11 13:26:27 +0300

    feat: test first request

--- resto-frontend/src/App.vue
+++ resto-frontend/src/App.vue
@@ -1,11 +1,17 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const message = ref('Loading...')
+
+onMounted(async () => {
+  const res = await fetch('http://127.0.0.1:8000/api/test')
+  const data = await res.json()
+  message.value = data.message
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>{{ message }}</h1>
 </template>
 
 <style scoped></style>
