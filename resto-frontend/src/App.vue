526783012b55d08b70bd3b329a9e9996c73d3d95
ailchenko <velma86@bk.ru>
2025-08-13 19:33:08 +0300

    feat: started work with tailwind

--- resto-frontend/src/App.vue
+++ resto-frontend/src/App.vue
@@ -1,33 +1,10 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-
-const message = ref('Loading...')
-const result = ref()
-const name = ref('Рататуй')
-
-onMounted(async () => {
-  const res = await fetch('http://127.0.0.1:8000/api/drinks')
-  const data = await res.json()
-  console.log(data.data)
-  result.value = data.data
-  message.value = data.message
-})
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <h1 class="header">{{ message ?? result[0].name }}</h1>
-  <img src="../public/images/logo.png" class="logo" />
+  <Header />
 </template>
 
-<style lang="scss" scoped>
-@use '../public/styles/main.scss' as styles;
-
-.logo {
-  width: 150px;
-  height: 150px;
-}
-
-.header {
-  color: styles.$accent-color;
-}
-</style>
+<style lang="scss" scoped></style>
--- resto-frontend/src/components/Header.vue
+++ resto-frontend/src/components/Header.vue
@@ -1,12 +1,33 @@
-<script lang="js"></script>
+<script setup lang="js">
+import { ref } from 'vue'
+import content from './../content/constants.json'
+import './../assets/styles/tailwind.css'
+
+const menu = ref([])
+const logoPath = ref('')
+menu.value = content.menu
+logoPath.value = content.logoPath
+console.log(menu.value)
+</script>
 <template>
-  <header>
-    <div class="logo"></div>
-    <nav>
-      <ul></ul>
+  <header class="flex mx-0 my-10">
+    <div class="logo w-3">
+      <img :src="logoPath" alt="alt" class="w-4" />
+    </div>
+    <div class="bg-red-500 w-10 h-10"></div>
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+      <ul class="flex lg:flex-1">
+        <li v-for="(item, i) in menu" :key="i">
+          <a
+            :href="item"
+            class="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100"
+            >{{ item }}</a
+          >
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
-<style lang="scss" scoped>
-@use '../public/styles/main.scss' as styles;
+<style lang="scss">
+@import './../assets/styles/tailwind.css';
 </style>
