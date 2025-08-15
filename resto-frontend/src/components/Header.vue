d493cd0e01bea01bfcea8fbf350312eec84fddbc
ailchenko <velma86@bk.ru>
2025-08-15 14:03:13 +0300

    feat: Header is done

--- resto-frontend/src/components/Header.vue
+++ resto-frontend/src/components/Header.vue
@@ -1,33 +1,37 @@
 <script setup lang="js">
 import { ref } from 'vue'
 import content from './../content/constants.json'
-import './../assets/styles/tailwind.css'
 
 const menu = ref([])
 const logoPath = ref('')
 menu.value = content.menu
 logoPath.value = content.logoPath
-console.log(menu.value)
 </script>
 <template>
-  <header class="flex mx-0 my-10">
-    <div class="logo w-3">
-      <img :src="logoPath" alt="alt" class="w-4" />
+  <header class="container flex mx-auto my-10 px-10 justify-around items-center bg-base">
+    <div class="logo basis-64">
+      <a href="/">
+        <img :src="logoPath" alt="alt" />
+      </a>
     </div>
-    <div class="bg-red-500 w-10 h-10"></div>
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-      <ul class="flex lg:flex-1">
+    <nav class="basis-128 mx-auto">
+      <ul class="flex max-w-9xl items-center justify-evenly">
         <li v-for="(item, i) in menu" :key="i">
           <a
             :href="item"
-            class="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100"
+            class="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 hover:text-middle-size font-semibold text-my-text hover:text-accent px-4 py-2 duration-300 w-[30px]"
             >{{ item }}</a
           >
         </li>
       </ul>
     </nav>
+    <span>
+      <a
+        href="cabinet"
+        class="basis-64 text-my-text hover:text-base hover:bg-accent border rounded-full border-accent-second px-6 py-3 duration-300"
+        >Login</a
+      >
+    </span>
   </header>
 </template>
-<style lang="scss">
-@import './../assets/styles/tailwind.css';
-</style>
+<style lang="scss"></style>
